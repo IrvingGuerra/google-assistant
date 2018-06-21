69bbf61b0e8ce3fcd541587bc00a7adefd92e1c7
IrvingGuerra <37481550+IrvingGuerra@users.noreply.github.com>
2018-06-21 13:23:26 -0500

    Finalizado con despedida

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ restService.post("/echo", function(req, res) {
 
           valor_lectura = result;
 
-          respuesta = Sensores + " en la " + Estacion + " es de "+ valor_lectura + " " + tipoValor;
+          respuesta = Sensores + " en la " + Estacion + " es de "+ valor_lectura + " " + tipoValor +". ¿Necesitas algo más? ";
 
           return res.json({
             fulfillmentText: respuesta,
