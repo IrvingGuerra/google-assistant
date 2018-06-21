7e5c2ee3f092aa9c4a6464754705c45451b55bd3
IrvingGuerra <37481550+IrvingGuerra@users.noreply.github.com>
2018-06-21 00:28:05 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -193,16 +193,7 @@ function Consulta1(id_est,id_sens){
        }
   );
 
-  var Sentencia3 = "SELECT value FROM registers WHERE id = '"+id_registro+"'";
 
-  connection.query(Sentencia3, function(error, result){
-          if(error){
-             throw error;
-          }else{
-            valor = result[0].value;
-          }
-       }
-  );
 
 }
 
